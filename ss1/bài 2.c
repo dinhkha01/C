8efc7aa5cbc26f6f451c8f9373a61ca0b4b5e91a
dinhkha01 <149988948+dinhkha01@users.noreply.github.com>
2023-12-01 16:25:48 +0700

    change code

---
+++ ss1/bài 2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(){
+    int a;
+    int b;
+    double nghiem;
+    printf("nhap he so a = ");
+    scanf("%d", &a);
+    printf("nhap he so b = ");
+    scanf("%d", &b);
+    printf("phuong trinh sau khi nhap: %dx + %d = 0",a,b);
+    if(a == 0) {
+        if(b == 0) {
+            printf("\n-> phuong trinh tren co vo so nghiem\n");
+        } else {
+            printf("\n-> phuong trinh tren vo nghiem\n");
+        }
+    }
+    else {
+        nghiem = (double)-b/a;
+        printf("\n-> phuong trinh tren co nghiem la: x = %0.2f\n",nghiem);
+    }
+    return 0;
+}
+//
+// Created by Admin on 12/1/2023.
+//
