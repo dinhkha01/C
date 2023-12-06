dc179262366f9767ff4ad3e23b39715a5c76a734
dinhkha01 <149988948+dinhkha01@users.noreply.github.com>
2023-12-06 22:32:00 +0700

    c

--- ss4/bai 4.c
+++ ss4/bai 4.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 int main () {
-    int t,n;
+    int year,month;
     printf("nhap vao nam va thang:   ");
-    scanf ("%d%d",&n,&t);
-    switch (t) {
+    scanf ("%d%d",&year,&month);
+    switch (month) {
     case 1 : case 3: case 5: case 7: case 8: case 10: case 12:
-        printf ( " thang %d la thang co 31 ngay\n",t);
+        printf ( " thang %d la thang co 31 day\n",month);
             break;
         case 4: case 6: case 9: case 11:
-            printf("thang %d la thang co 30 ngay\n",t);
+            printf("thang %d la thang co 30 day\n",month);
             break;
         case 2:
-            if ((n % 4 == 0) && (n % 100!= 0) || (n%400 == 0))
-                printf("thang %d la thang co 29 ", t);
+            if ((year % 4 == 0) && (year % 100!= 0) || (year%400 == 0))
+                printf("thang %d la thang co 29 day ", month);
             else
-                printf("thang %d la thang co 28", t);
+                printf("thang %d la thang co 28 day ", month);
     }
     return 0;
 
