17499ae3e7783f6481af81ac715fd34a6ce0befc
dinhkha01 <149988948+dinhkha01@users.noreply.github.com>
2023-12-06 09:32:10 +0700

    change code

--- ss4/bai 4.c
+++ ss4/bai 4.c
@@ -6,17 +6,16 @@ int main () {
     switch (t) {
     case 1 : case 3: case 5: case 7: case 8: case 10: case 12:
         printf ( " thang %d la thang co 31 ngay\n",t);
+            break;
         case 4: case 6: case 9: case 11:
             printf("thang %d la thang co 30 ngay\n",t);
+            break;
         case 2:
             if ((n % 4 == 0) && (n % 100!= 0) || (n%400 == 0))
-                printf("%d la mot nam nhuan", n);
+                printf("thang %d la thang co 29 ", t);
             else
-                printf("%d khong phai la nam nhuan", n);
-
+                printf("thang %d la thang co 28", t);
     }
-
-
     return 0;
 
 }
